1e84fa853cd327c40741337a2ecd3a3f5191ec71
wangxiuqiang <wangxiuqianga@163.com>
2017-07-18 10:30:36 +0800

    Practice

--- a/src/main/java/controller/InputProductController.java
+++ a/src/main/java/controller/InputProductController.java
@@ -1,5 +1,19 @@
 package controller;
 
-public class InputProductController {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class InputProductController implements Controller {
+    private final static Log logger = LogFactory.getLog(InputProductController.class);
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		logger.info("InputProductController called");
+		
+		return new ModelAndView("/src/main/webapp/WEB-INF/jsp/ProductForm.jsp");
+	}
 
 }
